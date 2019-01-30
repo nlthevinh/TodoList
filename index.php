d58f7d570f40f44522831172d17169e8df66aaa6
nlthevinh <46483319+nlthevinh@users.noreply.github.com>
2019-01-30 22:46:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,22 +13,31 @@
     <body>
         <h1>Todo List</h1>
         <?php
-			$resultat=$db->selection('SELECT * FROM todos');
-			$todos=$resultat->fetchAll();
-			foreach($todos as $todo){
+			if($action='afficher'){
+				echo "Ajouter tâches";
+				$resultat=$db->selection('SELECT * FROM todos');
+				$todos=$resultat->fetchAll();
+				foreach($todos as $todo){
 				echo"<table><tr><td>N°</td><td>Tâche</td></tr><tr><td>$todo[id]</td><td>$todo[texte]</td></tr></table>";
 			};
-            if(isset($_POST['texte'])){
-				echo "Tâche ajouté ".$_POST['texte'];
+			}
+            elseif(isset($_POST['texte'])&&$action='ajouter'){
+				echo "Tâche ajoutée ".$_POST['texte'];
 				$db->insertion('INSERT INTO todos (texte) VALUES ("$_POST[\'texte\']")');
             }
+			elseif($action='supprimer'){
+				echo "Tâches supprimées";
+				$db->suppression('DELETE FROM todos');
+			}
             else{
 				echo "Entrez une tâche";
             }
         ?>
         <form method="post" action="index.php">
             <input type="text" name="texte"/>
+			<input type="submit" value="afficher"/>
             <input type="submit" value="ajouter"/>
+			<input type="submit" value="supprimer"/>
         </form>
     </body>
 </html>
